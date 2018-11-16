3c7847582a9a406689ef4a97b48dad70c158f331
WillCPP <WillCPP@users.noreply.github.com>
2018-11-16 15:09:35 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,177 @@
+var element = document.querySelector(".animate");
+var element0 = document.querySelector(".animate0");
+var element1 = document.querySelector(".animate1");
+var element2 = document.querySelector(".animate2");
+var element3 = document.querySelector(".animate3");
+var element4 = document.querySelector(".animate4");
+var element5 = document.querySelector(".animate5");
+var element6 = document.querySelector(".animate6");
+
+window.addEventListener("scroll", function() {
+  if(this.pageYOffset > 0) {
+        if(element.classList.contains("back-blue")){
+                element.classList.remove("back-blue");
+                element.classList.add("back-red");
+        }else if(element.classList.contains("back-red")){
+                element.classList.remove("back-red");
+                element.classList.add("back-blue");
+        }else if(element.classList.contains("back-green")){
+                element.classList.remove("back-green");
+                element.classList.add("back-yellow");
+        }else if(element.classList.contains("back-yellow")){
+                element.classList.remove("back-yellow");
+                element.classList.add("back-green");
+        }else if(element.classList.contains("back-pink")){
+                element.classList.remove("back-pink");
+                element.classList.add("back-orange");
+        }else if(element.classList.contains("back-orange")){
+                element.classList.remove("back-orange");
+                element.classList.add("back-pink");
+        }}});
+window.addEventListener("scroll", function() {
+  if(this.pageYOffset > 0) {
+	if(element0.classList.contains("back-blue")){
+		element0.classList.remove("back-blue");
+		element0.classList.add("back-red");
+	}else if(element0.classList.contains("back-red")){
+		element0.classList.remove("back-red");
+                element0.classList.add("back-blue");
+	}else if(element0.classList.contains("back-green")){
+		element0.classList.remove("back-green");
+                element0.classList.add("back-yellow");
+	}else if(element0.classList.contains("back-yellow")){
+		element0.classList.remove("back-yellow");
+                element0.classList.add("back-green");
+	}else if(element0.classList.contains("back-pink")){
+		element0.classList.remove("back-pink");
+                element0.classList.add("back-orange");
+	}else if(element0.classList.contains("back-orange")){
+		element0.classList.remove("back-orange");
+                element0.classList.add("back-pink");
+	}}});
+window.addEventListener("scroll", function() {
+  if(this.pageYOffset > 0) {
+        if(element1.classList.contains("back-blue")){
+                element1.classList.remove("back-blue");
+                element1.classList.add("back-red");
+        }else if(element1.classList.contains("back-red")){
+                element1.classList.remove("back-red");
+                element1.classList.add("back-blue");
+        }else if(element1.classList.contains("back-green")){
+                element1.classList.remove("back-green");
+                element1.classList.add("back-yellow");
+        }else if(element1.classList.contains("back-yellow")){
+                element1.classList.remove("back-yellow");
+                element1.classList.add("back-green");
+        }else if(element1.classList.contains("back-pink")){
+                element1.classList.remove("back-pink");
+                element1.classList.add("back-orange");
+        }else if(element1.classList.contains("back-orange")){
+                element1.classList.remove("back-orange");
+                element1.classList.add("back-pink");
+        }}});
+window.addEventListener("scroll", function() {
+  if(this.pageYOffset > 0) {
+        if(element2.classList.contains("back-blue")){
+                element2.classList.remove("back-blue");
+                element2.classList.add("back-red");
+        }else if(element2.classList.contains("back-red")){
+                element2.classList.remove("back-red");
+                element2.classList.add("back-blue");
+        }else if(element2.classList.contains("back-green")){
+                element2.classList.remove("back-green");
+                element2.classList.add("back-yellow");
+        }else if(element2.classList.contains("back-yellow")){
+                element2.classList.remove("back-yellow");
+                element2.classList.add("back-green");
+        }else if(element2.classList.contains("back-pink")){
+                element2.classList.remove("back-pink");
+                element2.classList.add("back-orange");
+        }else if(element2.classList.contains("back-orange")){
+                element2.classList.remove("back-orange");
+                element2.classList.add("back-pink");
+        }}});
+window.addEventListener("scroll", function() {
+  if(this.pageYOffset > 0) {
+        if(element3.classList.contains("back-blue")){
+                element3.classList.remove("back-blue");
+                element3.classList.add("back-red");
+        }else if(element3.classList.contains("back-red")){
+                element3.classList.remove("back-red");
+                element3.classList.add("back-blue");
+        }else if(element3.classList.contains("back-green")){
+                element3.classList.remove("back-green");
+                element3.classList.add("back-yellow");
+        }else if(element3.classList.contains("back-yellow")){
+                element3.classList.remove("back-yellow");
+                element3.classList.add("back-green");
+        }else if(element3.classList.contains("back-pink")){
+                element3.classList.remove("back-pink");
+                element3.classList.add("back-orange");
+        }else if(element3.classList.contains("back-orange")){
+                element3.classList.remove("back-orange");
+                element3.classList.add("back-pink");
+        }}});
+window.addEventListener("scroll", function() {
+  if(this.pageYOffset > 0) {
+        if(element4.classList.contains("back-blue")){
+                element4.classList.remove("back-blue");
+                element4.classList.add("back-red");
+        }else if(element4.classList.contains("back-red")){
+                element4.classList.remove("back-red");
+                element4.classList.add("back-blue");
+        }else if(element4.classList.contains("back-green")){
+                element4.classList.remove("back-green");
+                element4.classList.add("back-yellow");
+        }else if(element4.classList.contains("back-yellow")){
+                element4.classList.remove("back-yellow");
+                element4.classList.add("back-green");
+        }else if(element4.classList.contains("back-pink")){
+                element4.classList.remove("back-pink");
+                element4.classList.add("back-orange");
+        }else if(element4.classList.contains("back-orange")){
+                element4.classList.remove("back-orange");
+                element4.classList.add("back-pink");
+        }}});
+window.addEventListener("scroll", function() {
+  if(this.pageYOffset > 0) {
+        if(element5.classList.contains("back-blue")){
+                element5.classList.remove("back-blue");
+                element5.classList.add("back-red");
+        }else if(element5.classList.contains("back-red")){
+                element5.classList.remove("back-red");
+                element5.classList.add("back-blue");
+        }else if(element5.classList.contains("back-green")){
+                element5.classList.remove("back-green");
+                element5.classList.add("back-yellow");
+        }else if(element5.classList.contains("back-yellow")){
+                element5.classList.remove("back-yellow");
+                element5.classList.add("back-green");
+        }else if(element5.classList.contains("back-pink")){
+                element5.classList.remove("back-pink");
+                element5.classList.add("back-orange");
+        }else if(element5.classList.contains("back-orange")){
+                element5.classList.remove("back-orange");
+                element5.classList.add("back-pink");
+        }}});
+window.addEventListener("scroll", function() {
+  if(this.pageYOffset > 0) {
+        if(element6.classList.contains("back-blue")){
+                element6.classList.remove("back-blue");
+                element6.classList.add("back-red");
+        }else if(element6.classList.contains("back-red")){
+                element6.classList.remove("back-red");
+                element6.classList.add("back-blue");
+        }else if(element6.classList.contains("back-green")){
+                element6.classList.remove("back-green");
+                element6.classList.add("back-yellow");
+        }else if(element6.classList.contains("back-yellow")){
+                element6.classList.remove("back-yellow");
+                element6.classList.add("back-green");
+        }else if(element6.classList.contains("back-pink")){
+                element6.classList.remove("back-pink");
+                element6.classList.add("back-orange");
+        }else if(element6.classList.contains("back-orange")){
+                element6.classList.remove("back-orange");
+                element6.classList.add("back-pink");
+        }}});
